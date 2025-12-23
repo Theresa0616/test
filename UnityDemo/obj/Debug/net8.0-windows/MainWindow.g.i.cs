505389cbf3d0081a4c78c85a0e84b2f0cb588a96
Theresa0616 <theresa020616@gmail.com>
2025-12-23 09:02:41 +0800

    提交1

--- UnityDemo/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ UnityDemo/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace UnityDemo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UnityDemo;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UnityDemo;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
